2049bb95c7e8cbb9747f4566d90748931986db71
amar-jay <64834413+amar-jay@users.noreply.github.com>
2023-07-15 12:34:42 +0000

    😴

--- mine/main.go
+++ mine/main.go
@@ -6,25 +6,27 @@ import (
 	"errors"
 	"fmt"
 	"io"
+	"io/fs"
 	"log"
 	"os"
+	"os/exec"
 	"strings"
 
 	"github.com/urfave/cli/v2"
 )
 
 var (
-	GIT_DIR = ".git"
+	GIT_DIR = "../.git"
 )
 
 func init() {
-	log.SetFlags(log.Lshortfile | log.LstdFlags)
+	log.SetFlags(log.Lshortfile)
 	dir := os.Getenv("GIT_DIR")
-	if dir == "" {
-		dir = ".git"
+	if dir != "" {
+		GIT_DIR = dir
 	}
 
-	if err := setDir(dir); err != nil {
+	if err := setDir(GIT_DIR); err != nil {
 		log.Fatalf("Error setting git-dir: %s", err)
 		return
 	}
@@ -68,14 +70,128 @@ func main() {
 				Name:    "cat-file",
 				Usage:   "Display the contents of a file",
 				Aliases: []string{"c"},
+				Flags: []cli.Flag{
+					&cli.BoolFlag{
+						Name:    "type",
+						Aliases: []string{"t"},
+						Usage:   "Show the object type",
+					},
+					&cli.BoolFlag{
+						Name:    "pretty-print",
+						Aliases: []string{"p"},
+						Usage:   "Pretty-print the contents of the object",
+					},
+				},
 				Action: func(c *cli.Context) error {
 					hashes := c.Args().Slice()
+					// println(hashes)
 					for _, hash := range hashes {
 						if err := catHash(hash); err != nil {
 							log.Fatalf("Error reading hash %s: %s", hash, err)
 						}
 					}
 
+					return nil
+				},
+			},
+			{
+				Name:    "hash-object",
+				Usage:   "Compute object ID and optionally creates a blob from a file",
+				Aliases: []string{"h"},
+				Flags: []cli.Flag{
+					&cli.BoolFlag{
+						Name:    "write",
+						Aliases: []string{"w"},
+						Usage:   "Actually write the object into the database",
+					},
+				},
+				Action: func(c *cli.Context) error {
+					println("hash-object")
+					return nil
+				},
+			},
+			{
+				Name:    "log",
+				Usage:   "Show commit logs",
+				Aliases: []string{"l"},
+				Action: func(c *cli.Context) error {
+					println("not implemented yet")
+					cmd := exec.Command("git", "log")
+					cmd.Stdout = os.Stdout
+					cmd.Stderr = os.Stderr
+					cmd.Stdin = os.Stdin
+
+					if err := cmd.Run(); err != nil {
+						log.Fatalf("Error running git log: %s", err)
+					}
+					return nil
+				},
+			},
+			{
+				Name:    "ls-tree",
+				Usage:   "Pretty-print a tree object",
+				Aliases: []string{"ls"},
+				Action: func(c *cli.Context) error {
+					// read .git/object/ directories
+					var dirs []fs.DirEntry
+					dirs, err := os.ReadDir(GIT_DIR + "/objects")
+					if err != nil {
+						log.Fatalf("Error reading objects directory: %s", err)
+					}
+
+					// read the content of each directory
+					log.Printf("Hash \t\t\t\t\t\t Type \t Name \t")
+					for _, dir := range dirs {
+						if len(dir.Name()) != 2 {
+							continue
+						}
+						dircontents, err := os.ReadDir(GIT_DIR + "/objects/" + dir.Name())
+						if err != nil {
+							log.Fatalf("Error reading directory %s: %s", dir.Name(), err)
+						}
+						for _, file := range dircontents {
+							if !file.IsDir() {
+								log.Printf("%s \t %s \t %s \t", dir.Name()+file.Name(), "blob", "file")
+							}
+
+							filepath := fmt.Sprintf("%s/objects/%s/%s", GIT_DIR, dir.Name(), file.Name())
+							content, err := os.ReadFile(filepath)
+							if err != nil {
+								log.Fatalf("Failed to read the object file %s\n", err)
+								break
+							}
+							f, err := zlib.NewReader(bytes.NewReader(content))
+							if err != nil {
+								log.Fatalf("Failed to create zlib reader: %s\n", err)
+							}
+							defer f.Close()
+
+							out := bytes.Buffer{}
+
+							_, err = io.Copy(&out, f)
+
+							if err != nil {
+								log.Fatalf("Failed to read the content of the object: %s\n", err)
+								break
+							}
+
+							split := strings.Split(out.String(), "\000")
+
+							body := split[1]
+							fmt.Printf("%s", body)
+						}
+					}
+					// read the content of each file
+					// print the content
+					// println("not implemented yet")
+					// cmd := exec.Command("git", "ls-tree", "-r", "HEAD")
+					// cmd.Stdout = os.Stdout
+					// cmd.Stderr = os.Stderr
+					// cmd.Stdin = os.Stdin
+
+					// if err := cmd.Run(); err != nil {
+					// 	log.Fatalf("Error running git ls-tree: %s", err)
+					// }
 					return nil
 				},
 			},
@@ -91,6 +207,19 @@ func main() {
 						log.Fatalf("Error setting git-dir: %s", err)
 					}
 
+					return nil
+				},
+			},
+			&cli.BoolFlag{
+				// verbose logging
+				Name:    "verbose",
+				Aliases: []string{"v"},
+				Usage:   "Verbose logging",
+				Value:   false,
+				Action: func(_ *cli.Context, v bool) error {
+					if v {
+						log.SetFlags(log.Lshortfile | log.LstdFlags)
+					}
 					return nil
 				},
 			},
